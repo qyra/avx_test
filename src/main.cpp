a40af5115c534c373108ce3cbac07214d16be0d7
Karl <karl9242@gmail.com>
2017-08-11 03:13:06 -0500

    Added notes for SSE2 version

--- src/main.cpp
+++ src/main.cpp
@@ -30,6 +30,7 @@ inline float hsum_m128_elements(__m128 v) {
 
 float sse_sum_of_squares(int n, const float* x, const float* y)
 {
+    float result;
     __m128 sum_set = _mm_setzero_ps();
 
     for (; n>3; n-=4) {
@@ -42,14 +43,15 @@ float sse_sum_of_squares(int n, const float* x, const float* y)
         y+=4;
     }
 
+    //~ //Do this if only SSE2 is available
     //~ const __m128 shuffle1 = _mm_shuffle_ps(sum, sum, _MM_SHUFFLE(1,0,3,2));
-    //~ const __m128 sum1 = _mm_add_ps(sum, shuffle1);
+    //~ const __m128 sum1 = _mm_add_ps(euclidean, shuffle1);
     //~ const __m128 shuffle2 = _mm_shuffle_ps(sum1, sum1, _MM_SHUFFLE(2,3,0,1));
     //~ const __m128 sum2 = _mm_add_ps(sum1, shuffle2);
-    // with SSE3, we could use hadd_ps, but the difference is negligible
+    //~ _mm_store_ss(&result,sum2);
 
     //~ The haddps instruction performs a horizontal add, meaning that adjacent elements in the same operand are added together. Each 128-bit argument is considered as four 32-bit floating-point elements, numbered from 0 to 3, with 3 being the high-order element. The result of the operation on operand a (A3, A2, A1, A0) and operand b (B3, B2, B1, B0) is (B3 + B2, B1 + B0, A3 + A2, A1 + A0).
-    float result = hsum_m128_elements(sum_set);
+    result = hsum_m128_elements(sum_set);
 
     if (n)
         result += sum_of_squares(n, x, y);    // remaining 1-3 entries
